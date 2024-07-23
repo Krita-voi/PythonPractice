8feec6d3bd87cd2d08746e149e628cc6c092af27
Krita-voi <kritagyamaharjan794@gmail.com>
2024-07-23 09:47:31 +0545

    first commit

---
+++ basic.py
@@ -0,0 +1,24 @@
+def sum(a,b):
+    return a+b 
+
+def sub(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+
+def modulus(a,b):
+    return a%b
+
+z=float(input("!"))
+y=float(input("?"))
+
+print(sum(z,y))
+print(sub(z,y))
+print(multiply(z,y))
+print(divide(z,y))
+# print(modulus(z,y))
+
